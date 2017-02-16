8f290c91027b4b9bac19dfd41d63578c7342be65
Strawing <liyafe1997@126.com>
2017-02-16 22:15:50 +0800

    Add files via upload

---
+++ DisableSafeMode/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace DisableSafeMode
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (SystemInformation.BootMode != BootMode.Normal)
+            {
+                Process.Start("shutdown.exe", "-r -f -t 0");
+            }
+            else
+            {
+                Thread thread = new Thread(new ThreadStart(StopInNormal));
+                thread.Start();
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public void StopInNormal()
+        {
+            Thread.Sleep(5000); 
+
+            System.Environment.Exit(0);
+        }
+    }
+}
